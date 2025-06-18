2bf3e4a06900bdcbf852cd60b47839db918ba3bc
lag129 <65485662+lag129@users.noreply.github.com>
2025-06-18 21:39:30 +0900

    LazyColumnのアイテム表示を修正

--- app/src/main/java/net/lag129/mastodon/ui/screens/TimelineScreen.kt
+++ app/src/main/java/net/lag129/mastodon/ui/screens/TimelineScreen.kt
@@ -1,18 +1,19 @@
 package net.lag129.mastodon.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import net.lag129.mastodon.ui.components.PostContent
 import net.lag129.mastodon.ui.components.RepostContent
 import net.lag129.mastodon.viewmodel.DataViewModel
@@ -22,27 +23,31 @@ fun TimelineScreen(
     viewModel: DataViewModel,
     modifier: Modifier = Modifier
 ) {
-    val statuses by remember { viewModel.statuses }.collectAsState()
-    val isLoading by remember { viewModel.isLoading }
+    val statuses by viewModel.statuses.collectAsState()
+    val isLoading by viewModel.isLoading
 
     LazyColumn(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(vertical = 16.dp),
     ) {
-        itemsIndexed(statuses, key = { _, item -> item.id }) { index, status ->
+        items(
+            items = statuses,
+            key = { status -> status.id },
+        ) { status ->
             if (status.reblog == null) {
                 PostContent(status)
             } else {
                 RepostContent(status)
             }
 
-            if (index >= statuses.size - 5 && !isLoading) {
-                LaunchedEffect(index) {
+            if (status == statuses.lastOrNull() && !isLoading) {
+                LaunchedEffect(status.id) {
                     viewModel.fetchNextPage()
                 }
             }
         }
         if (isLoading) {
-            item {
+            item(key = "loading") {
                 LoadingIndicator(viewModel, Modifier.fillMaxWidth())
             }
         }
